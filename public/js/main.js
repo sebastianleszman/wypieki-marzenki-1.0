772473017264ba27e9913e5bd647c77993c855b5
Sebastian Leszman <sebastianleszman@gmail.com>
2020-06-16 17:19:48 +0200

    another commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,53 @@
+// header //
+
+const mainHeader = document.querySelector('.nav-scroll');
+let last = 0;
+
+window.addEventListener('scroll', function () {
+  let scroll = document.documentElement.scrollTop;
+
+  setTimeout(function () {
+    if (scroll > last) {
+      mainHeader.classList.add('hide');
+    } else {
+      mainHeader.classList.remove('hide');
+    }
+    last = scroll;
+  }, 100);
+});
+
 // Navbar
 
-const menuToggleBtn = document.querySelector(".main-header__nav-toggle-btn");
-const mainNav = document.querySelector(".main-header__nav");
+const menuToggleBtn = document.querySelector('.main-header__nav-toggle-btn');
+const mainNav = document.querySelector('.main-header__nav');
 
 function menuToggleClickHandler() {
-  mainNav.classList.toggle("menu--active");
-  menuToggleBtn.classList.toggle("hamburger--active");
+  mainNav.classList.toggle('menu--active');
+  menuToggleBtn.classList.toggle('hamburger--active');
 }
 
-menuToggleBtn.addEventListener("click", menuToggleClickHandler);
+menuToggleBtn.addEventListener('click', menuToggleClickHandler);
 
-document.addEventListener("keydown", function (event) {
-  if (mainNav.classList.contains("menu--active")) {
-    if (event.key === "Escape") {
-      mainNav.classList.remove("menu--active");
-      menuToggleBtn.classList.remove("hamburger--active");
+document.addEventListener('keydown', function (event) {
+  if (mainNav.classList.contains('menu--active')) {
+    if (event.key === 'Escape') {
+      mainNav.classList.remove('menu--active');
+      menuToggleBtn.classList.remove('hamburger--active');
     }
   }
 });
 
-
-
 // Gallery
 
-const lightbox = document.createElement("div");
-lightbox.id = "lightbox";
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 
-const images = document.querySelectorAll("img");
+const images = document.querySelectorAll('img');
 images.forEach((image) => {
-  image.addEventListener("click", (e) => {
-    lightbox.classList.add("active");
-    const img = document.createElement("img");
+  image.addEventListener('click', (e) => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
     img.src = image.src;
     while (lightbox.firstChild) {
       lightbox.removeChild(lightbox.firstChild);
@@ -40,7 +56,7 @@ images.forEach((image) => {
   });
 });
 
-lightbox.addEventListener("click", (e) => {
+lightbox.addEventListener('click', (e) => {
   if (e.target !== e.currentTarget) return;
-  lightbox.classList.remove("active");
+  lightbox.classList.remove('active');
 });
